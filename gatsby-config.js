7e51b5e9aa865c69058c7d3710199fba0d1065e4
Michael Helmers <mikehelmers@protonmail.com>
2022-05-03 11:18:07 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Mike and Tech`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Michael Helmers`,
+      summary: `who lives and works in Madison building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `mphelmers`,
     },
   },
   plugins: [
